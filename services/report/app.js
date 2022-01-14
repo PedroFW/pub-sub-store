933807d80f6859f2757aeaa15d2039885a599e88
Pedro <0wfonsecapedro@gmail.com>
2022-01-14 14:20:13 -0300

    report

--- services/report/app.js
+++ services/report/app.js
@@ -25,6 +25,9 @@ async function printReport() {
 
 async function consume() {
     //TODO: Constuir a comunicação com a fila 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (products) => {updateReport(products)})
+    printReport()
 } 
 
 consume()
